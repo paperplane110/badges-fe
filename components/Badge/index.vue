4050cfd77aa72c44ea449c486c50851c3b84783b
paperplane110 <1374736640@qq.com>
2023-03-02 21:39:26 +0800

    Fix: badge doom flicker

--- components/Badge/index.vue
+++ components/Badge/index.vue
@@ -1,40 +1,30 @@
 <template>
-    <div class="flex justify-center items-center h-80px w-80px rounded-1 text-5xl shadow shadow-md transition duration-1000 hover:(transform rotate-y-360)"
-        :class="[props.bgColor, 'filter', saturateEffect, opacityEffect]">
-        {{ props.pattern }}
+  <div class="background">
+    <div class="badge flex justify-center items-center h-80px w-80px rounded-1 text-5xl shadow shadow-md select-none"
+      :class="[props.bgColor]">
+      {{ props.pattern }}
     </div>
+  </div>
 </template>
 
 <script setup lang='ts'>
 type badgeProps = {
-    pattern: string
-    bgColor: string
-    progress?: number
+  pattern: string
+  bgColor: string
+  progress?: number
 }
 
 const props = defineProps<badgeProps>()
-
-const saturateEffect = computed(() => {
-    let percent: number
-    if (props.progress !== undefined) {
-        percent = props.progress
-    }
-    else {
-        percent = 100
-    }
-
-    return `saturate-${percent}`
-})
-
-const opacityEffect = computed(() => {
-    let percent: number
-    if (props.progress !== undefined) {
-        percent = 60 + Math.ceil(props.progress * 0.4)
-    }
-    else {
-        percent = 100
-    }
-
-    return `opacity-${percent}`
-})
 </script>
+
+<style scoped>
+.badge {
+
+  transition: transform 500ms;
+}
+
+.background:hover .badge {
+  transition: transform 1000ms;
+  transform: rotateY(360deg);
+}
+</style>
